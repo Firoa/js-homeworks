a39e38763370ca6f6af8431abe6f036557a05a68
Oleksandr <in200pac1lab@gmail.com>
2019-09-06 12:06:43 +0300

    add prettierrc

---
+++ hw-01/app.js
@@ -0,0 +1,15 @@
+"use strict"
+const name = 'Генератор захисного поля';
+const price = 1000;
+let message ;
+
+message = "Вибраний ${name}, ціна за штуку ${price} кредитів"
+
+console.log(message);
+
+
+
+
+
+
+
