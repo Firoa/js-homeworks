9d6d4c50d787a407e5969b5ee57ee22432ba0bb2
Sasha <in200pac1lab@gmail.com>
2019-09-17 22:49:37 +0300

    hw-03-task2 add

--- hw-03/task_2.js
+++ hw-03/task_2.js
@@ -1,2 +1,7 @@
 'use strict';
 
+const countProps = object => Object.keys(object).length;
+
+console.log(countProps({})); // 0
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
