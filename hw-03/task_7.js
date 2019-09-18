d6ce5e98305055f03fc957e5e29f3af243294944
Sasha <in200pac1lab@gmail.com>
2019-09-18 22:39:51 +0300

    hw-03 add task3-6

--- hw-03/task_7.js
+++ hw-03/task_7.js
@@ -1,2 +1,66 @@
 'use strict';
+/*
+ * Типов транзацкий всего два.
+ * Можно положить либо снять деньги со счета.
+ */
+const Transaction = {
+    DEPOSIT: 'deposit',
+    WITHDRAW: 'withdraw',
+  };
+  
+  /*
+   * Каждая транзакция это объект со свойствами: id, type и amount
+   */
 
+const account = {
+    // Текущий баланс счета
+  balance: 0,
+
+  // История транзакций
+  transactions: [],
+
+  /*
+   * Метод отвечающий за добавление суммы к балансу
+   * Создает объект транзакции и вызывает addTransaction
+   */
+  deposit(amount) {
+      balance += amount;
+      this.addTransaction(amount);
+  },
+
+  /*
+   * Метод отвечающий за снятие суммы с баланса.
+   * Создает объект транзакции и вызывает addTransaction
+   *
+   * Если amount больше чем текущий баланс, выводи сообщение
+   * о том, что снятие такой суммы не возможно, недостаточно средств.
+   */
+  withdraw(amount) {},
+
+  /*
+   * Метод добавляющий транзацию в свойство transactions
+   * Принимает объект трназкции
+   */
+  addTransaction(transaction) {},
+
+  /*
+   * Метод возвращает текущий баланс
+   */
+  getBalance() {
+      return this.balance;
+  },
+
+  /*
+   * Метод ищет и возвращает объект транзации по id
+   */
+  getTransactionDetails(id) {},
+
+  /*
+   * Метод возвращает количество средств
+   * определенного типа транзакции из всей истории транзакций
+   */
+  getTransactionTotal(type) {},
+
+};
+
+console.log (account.getBalance  ());
