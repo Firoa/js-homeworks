2f998d0d6df96377f3473aaabd9db0c5c065a93f
Sasha <in200pac1lab@gmail.com>
2019-09-22 14:10:37 +0300

    task_6 rework

--- hw-03/task_6.js
+++ hw-03/task_6.js
@@ -2,19 +2,21 @@
 
 const products = [
   { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Радар', price: 1300, quantity: 6 },
   { name: 'Сканер', price: 2700, quantity: 3 },
   { name: 'Дроид', price: 400, quantity: 7 },
   { name: 'Захват', price: 1200, quantity: 2 },
 ];
 
 const calculateTotalPrice = function(arr, productName) {
+  let totalPrice = 0;
   for (const singleObject of arr) {
     if (singleObject.name === productName) {
-      return singleObject.price * singleObject.quantity;
+      totalPrice += singleObject.price * singleObject.quantity;
     }
   }
+  return totalPrice;
 };
 
 console.log(calculateTotalPrice(products, 'Радар')); // 5200
-
 console.log(calculateTotalPrice(products, 'Дроид')); // 2800
