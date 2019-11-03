7689d20db01837bcaed632e2588b6a279a3bc5ae
Sasha <in200pac1lab@gmail.com>
2019-11-04 00:14:30 +0200

    begin hw-07

--- hw-07/task_1.js
+++ hw-07/task_1.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const categories = document.querySelector('#categories');
+const countOfChildrens = categories.children.length;
+const childrens = categories.children;
+const title = childrens[0].firstElementChild.textContent;
+console.log('Count of categories:', countOfChildrens);
+console.log('categories:', categories);
+console.log(childrens);
+console.log(title);
+
+const tittleText = categories.querySelectorAll("h2");
+const buf = [...tittleText];
+console.log(buf.map(e=>e.textContent));
+const getTitle = ([...lists]) => {
+  console.log("List inside func:",lists);
+   console.log(lists[0].firstElementChild.textContent);
+  return lists.map(list => list.firstElementChild.textContent);
+};
+console.log(getTitle(childrens));
