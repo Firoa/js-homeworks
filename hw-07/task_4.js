2570af7af99fcbbb59beab06ccc7ea6192b7ae6f
Sasha <in200pac1lab@gmail.com>
2019-11-13 21:20:33 +0200

    hw-07

--- hw-07/task_4.js
+++ hw-07/task_4.js
@@ -1,2 +1,10 @@
 'use strict';
-
+let Generatedvalue = 0;
+const valueOutput = document.querySelector('#value');
+valueOutput.textContent = Generatedvalue;
+document
+  .querySelector("button[data-action='decrement']")
+  .addEventListener('click', () => valueOutput.textContent--);
+document
+  .querySelector("button[data-action='increment']")
+  .addEventListener('click', () => valueOutput.textContent++);
--- hw-07/task_5.js
+++ hw-07/task_5.js
@@ -1,2 +1,8 @@
 'use strict';
 
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+input.addEventListener(
+  'input',
+  event => (output.textContent = event.currentTarget.value),
+);
